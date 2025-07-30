b38136bf57b1e854e32d983841896aae6febd8f2
etiLiza <stolyarova_ea@mail.ru>
2025-07-30 13:28:19 +0700

    создано окно и виджеты

--- cats.py
+++ cats.py
@@ -1,1 +1,29 @@
-from tkinter import*
+from tkinter import *
+from PIL import Image? ImageTk
+import requests
+from io import BytesIO
+
+
+def loade_image():#
+    try:
+        response = requests.get()#делаем запрос по ссылке.кладем в переменную респонс
+        response.raise_for_status()# если будет какая то ошибка то мы получим её вэтой строке
+        image_data = BytesIO(response.content)#в эту переменную положим обработанную картинку
+
+
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img #присваиваем картинку лайблу чтобы сборщик мусора его не удалил
+
+window.mainloop()
