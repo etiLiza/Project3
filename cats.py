aae65d6fbca9f28e0bc89c3e1588edea3ad762e7
etiLiza <stolyarova_ea@mail.ru>
2025-07-30 19:36:08 +0700

    добавлен комбобокс для вывода тэгов. поле энтри удалено

--- cats.py
+++ cats.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+Allowed_tags = ['sleep','jump','fight','black','white','cute',]#это список поэтому в квадратных скобках
 
 
 def load_image(url):#
@@ -18,7 +21,7 @@ def load_image(url):#
         return None
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
     if img:
@@ -37,12 +40,6 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
-
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
@@ -53,5 +50,15 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
+
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)#в ттк очень много виджетов
+tag_combobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
