0c34ce3c5ba035cef7e12a9cf30f9bf838537d72
etiLiza <stolyarova_ea@mail.ru>
2025-07-30 14:03:02 +0700

    добавили кнопку для загрузки изображения

--- cats.py
+++ cats.py
@@ -1,12 +1,12 @@
 from tkinter import *
-from PIL import Image? ImageTk
+from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
 
-def loade_image():#
+def load_image(url):#
     try:
-        response = requests.get()#делаем запрос по ссылке.кладем в переменную респонс
+        response = requests.get(url)#делаем запрос по ссылке.кладем в переменную респонс
         response.raise_for_status()# если будет какая то ошибка то мы получим её вэтой строке
         image_data = BytesIO(response.content)#в эту переменную положим обработанную картинку
         img = Image.open(image_data)# из библиотеки пилой
@@ -15,6 +15,13 @@ def loade_image():#
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img  # присваиваем картинку лайблу чтобы сборщик мусора его не удалил
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -22,11 +29,9 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
 
-if img:
-    label.config(image=img)
-    label.image = img #присваиваем картинку лайблу чтобы сборщик мусора его не удалил
+url = "https://cataas.com/cat"
+set_image()
 
 window.mainloop()
