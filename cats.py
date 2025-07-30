b2b1d34015f5e376edf34414fb5912a64190120f
etiLiza <stolyarova_ea@mail.ru>
2025-07-30 18:33:00 +0700

    теперь каждое изображение открывается в новом окне

--- cats.py
+++ cats.py
@@ -17,11 +17,14 @@ def load_image(url):#
         print(f"Произошла ошибка: {e}")
         return None
 
-def set_image():
+def open_new_window():
     img = load_image(url)
-
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img  # присваиваем картинку лайблу чтобы сборщик мусора его не удалил
 
 
@@ -32,19 +35,15 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
 
-label = Label()
-label.pack()
-
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar,tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="загрузить фото", command=set_image)
+file_menu.add_command(label="загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
-set_image()
 
 window.mainloop()
